c19e6c1ca22920b5659c95a4ece54187d0871370
abs15986 <abs15986@gmail.com>
2018-04-18 12:31:04 +0800

    update

--- kcwiki_quotes.py
+++ kcwiki_quotes.py
@@ -7,22 +7,25 @@ import json
 import requests
 
 from copy import deepcopy
-
+from requests.exceptions import ConnectionError
 from collections import OrderedDict
 
-seasonal_suffix = 'Leyte' # kc3 的前缀
-seasonal_suffixs = [
-    'EveOfBattle2018', 'EveOfBattle' # kcwiki的语音的前缀，注意优先级越详细的放到前面
-]
 
-subtitles_api = 'https://api.kcwiki.moe/subtitles/detail'
-quotes_size_url = 'https://raw.githubusercontent.com/KC3Kai/KC3Kai/master/src/data/quotes_size.json'
-quotes_url = 'https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/scn/quotes.json'
-seasonal_url = 'https://zh.kcwiki.org/index.php?title=舰娘百科:语音字幕&action=raw'
-kcdata_url = 'https://kcwikizh.github.io/kcdata/ship/all.json'
+seasonal_suffix = ''
+seasonal_suffixs = []
 
-kcdata_json = {}
-seasonal_wikicode = ''
+with open('./config.json', 'r', encoding='utf-8') as fp:
+    config = json.load(fp)
+    seasonal_suffix = config['kc3_seasonal_suffix']
+    seasonal_suffixs = config['kcwiki_seasonal_suffixs']
+
+
+def print_help():
+    print('[kcwiki_quotes.py 使用帮助]\n')
+    print('>> python kcwiki_quotes.py --fetch\n?> 获取最新的季节性语音、通常语音以及最新的quotes.json和quotes_size.json\n')
+    print('>> python kcwiki_quotes.py --update\n?> 更新quotes.json文件\n')
+    print('* 遇到Error请不要慌张，注意读报错再做判断！ *')
+
 
 name2VoiceId = {
     'Intro': 1, 'Sec1': 2, 'Sec2': 3, 'Sec3': 4, 'ConstComplete': 5,
@@ -57,20 +60,30 @@ desc2Id = {}
 for k, v in id2Desc.items():
     desc2Id[v] = str(k)
 
-print('Fetching kcdata json for all ships...')
-resp = requests.get(kcdata_url)
-kcdata_json = resp.json()
-print('Kcdata fetched successfully!')
-
-print('Fetching "quotes.json"...')
-resp = requests.get(quotes_url)
-quotes_json = resp.json()
-with open('quotes.json', 'w', encoding='utf-8') as fp:
-    json.dump(quotes_json, fp, ensure_ascii=False, indent=2)
-print('"quotes.json" fetched successfully!')
+archive_pattern = re.compile(r'[0-9a-z]+-([0-9A-Za-z]+)')
 
 
-def prepareSubtitles(subtitles):
+def filter_suffix(raw_text):
+    suffix_set = set()
+    text_tokens = raw_text.split()
+    for token in text_tokens:
+        if token and type(token) is str:
+            re_result = re.match(archive_pattern, token)
+            if re_result:
+                archive_name = re_result.group(1)
+                for prefix in name2VoiceId.keys():
+                    if archive_name.startswith(prefix):
+                        suffix = archive_name.lstrip(prefix)
+                        if suffix:
+                            suffix_set.add(suffix)
+                        break
+    print('[4.1] 共获得以下季节性后缀：')
+    suffix_list = list(suffix_set)
+    print(json.dumps(suffix_list, ensure_ascii=False, indent=2))
+    print('[4.2] 请酌情添加入 config.json 的 "kcwiki_seasonal_suffixs" 数组中。')
+
+
+def pre_subtitles(subtitles):
     res = {}
     for shipId in subtitles:
         res[shipId] = {}
@@ -79,15 +92,13 @@ def prepareSubtitles(subtitles):
     return res
 
 
-def updateSubtitles(): # 通常语音更新
+def update_subtitles():
     subtitles = {}
     quotes = {}
-    with open('quotes.json', 'r', encoding='utf-8') as fp:
+    with open('./quotes.json', 'r', encoding='utf-8') as fp:
         quotes = json.load(fp)
-    resp = requests.get(subtitles_api)
-    subtitles = prepareSubtitles(resp.json())
-    with open('./kcwiki.json', 'w', encoding='utf_8') as fp:
-        json.dump(subtitles, fp, indent=2, ensure_ascii=False)
+    with open('./subtitles.json') as fp:
+        subtitles = json.load(fp)
     for ship_id, voices in subtitles.items():
         s_id = str(ship_id)
         if type(voices) != dict:
@@ -100,31 +111,59 @@ def updateSubtitles(): # 通常语音更新
                 continue
             content = content.strip()
             quotes[s_id][id2Desc[v_id]] = content
-    with open('quotes.json', 'w', encoding='utf-8') as fp:
+    with open('./quotes.json', 'w', encoding='utf-8') as fp:
         json.dump(quotes, fp, ensure_ascii=False,
                   indent=2)
 
 
-def updateSeasonal(suffix=None): # 季节性语音更新
-    if not suffix:
-        print('Skip seasonal!')
-        return
+def fetch_data():
+    print('[-] 正在获取最新的数据……')
+    session = requests.Session()
+    subtitles_url = 'http://api.kcwiki.org/subtitles/detail'
+    quotes_size_url = 'https://raw.githubusercontent.com/KC3Kai/KC3Kai/master/src/data/quotes_size.json'
+    quotes_url = 'https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/scn/quotes.json'
+    seasonal_url = 'http://zh.kcwiki.org?title=%E8%88%B0%E5%A8%98%E7%99%BE%E7%A7%91:%E8%AF%AD%E9%9F%B3%E5%AD%97%E5%B9%95&action=raw'
+    kcdata_url = 'http://kcwikizh.github.io/kcdata/ship/all.json'
+    try:
+        subtitles = session.get(subtitles_url).json()
+        with open('./subtitles.json', 'w', encoding='utf-8') as fp:
+            json.dump(pre_subtitles(subtitles), fp, ensure_ascii=False)
+        print('[1] subtitles.json 获取成功！')
+        quotes_size = session.get(quotes_size_url).json()
+        with open('./quotes_size.json', 'w', encoding='utf-8') as fp:
+            json.dump(quotes_size, fp, ensure_ascii=False)
+        print('[2] quotes_size.json 获取成功！')
+        quotes = session.get(quotes_url).json()
+        with open('./quotes.json', 'w', encoding='utf-8') as fp:
+            json.dump(quotes, fp, ensure_ascii=False)
+        print('[3] quotes.json 获取成功！')
+        seasonal = session.get(seasonal_url).text
+        filter_suffix(seasonal)
+        with open('./seasonal.txt', 'w', encoding='utf-8') as fp:
+            fp.write(seasonal)
+        print('[4] seasonal.txt 获取成功！')
+        kcdata = session.get(kcdata_url).json()
+        with open('./kcdata.json', 'w', encoding='utf-8') as fp:
+            json.dump(kcdata, fp, ensure_ascii=False)
+        print('[5] kcdata.json 获取成功！')
+    except ConnectionError:
+        print('不要慌，只是网络问题，重试就好了！')
+    finally:
+        session.close()
+    print('[Y] 全部数据获取成功！')
+
+
+def update_seasonal():
     wikiId2apiId = {}
     quotes = {}
-    print('Fetching kcwiki seasonal wikicode "%s" suffix...' % suffix)
-    resp = requests.get(seasonal_url)
-    with open('{}.txt'.format(suffix), 'w', encoding='utf-8') as fp:
-        fp.write(resp.text)
-    print('Wikicode fetched successfully!')
-
+    quotes_size_json = {}
+    kcdata_json = []
+    with open('./kcdata.json') as fp:
+        kcdata_json = json.load(fp)
     for ship in kcdata_json:
         wikiId2apiId[ship['wiki_id']] = ship['id']
-
     voice_datajson = {}
-    if os.path.isfile('{}.json'.format(suffix)):
-        with open('{}.json'.format(suffix), encoding='utf-8') as fp:
-            voice_datajson = json.load(fp)
-    with open('{}.txt'.format(suffix), 'r', encoding='utf-8') as fp:
+    with open('./seasonal.txt', 'r', encoding='utf-8') as fp:
         line = fp.readline()
         cur_shipid = None
         cur_voiceid = None
@@ -158,40 +197,39 @@ def updateSeasonal(suffix=None): # 季节性语音更新
                 cur_voiceid = None
             line = fp.readline()
 
-    with open('{}.json'.format(suffix), 'w', encoding='utf-8') as fp:
-        json.dump(voice_datajson, fp, ensure_ascii=False,
-                  indent=4, sort_keys=True)
-
-    print('Fetching "quotes_size.json"...')
-    resp = requests.get(quotes_size_url)
-    quotes_size_json = resp.json()
-    print('"quotes_size_json" fetched successfully!')
-
-    with open('quotes.json', 'r', encoding='utf-8') as fp:
+    with open('./quotes.json', 'r', encoding='utf-8') as fp:
         quotes = json.load(fp)
 
+    with open('./quotes_size.json', 'r', encoding='utf-8') as fp:
+        quotes_size_json = json.load(fp)
+
     for api_id, voices in voice_datajson.items():
+        if api_id not in quotes_size_json:
+            continue
         qv_keys = quotes_size_json[api_id].keys()
         for v_id, content in voices.items():
             if v_id in qv_keys:
                 seasonal_keys = list(quotes_size_json[api_id][v_id].values())
                 desc = id2Desc[int(v_id)]
                 _content = content.strip()
-                seasonal_vid = '{}@{}'.format(
-                    v_id, suffix)
-                if suffix in seasonal_keys:
+                vid_seasonal = '{}@{}'.format(
+                    v_id, seasonal_suffix)
+                if seasonal_suffix in seasonal_keys:
                     if desc in quotes[api_id] and quotes[api_id][desc] == _content:
                         continue
-                    quotes[api_id][seasonal_vid] = _content
+                    quotes[api_id][vid_seasonal] = _content
 
     with open('quotes.json', 'w', encoding='utf-8') as fp:
         json.dump(quotes, fp, ensure_ascii=False,
                   indent=2)
 
 
-def minifyAll():
+def minify_all():
     quotes = {}
-    with open('quotes.json', 'r', encoding='utf-8') as fp:
+    kcdata_json = []
+    with open('./kcdata.json') as fp:
+        kcdata_json = json.load(fp)
+    with open('./quotes.json', 'r', encoding='utf-8') as fp:
         quotes = json.load(fp)
     quotes2 = deepcopy(quotes)
     for ship_info in kcdata_json:
@@ -218,12 +256,29 @@ def minifyAll():
                         if ship_id != next_id and v_id == v2_id and content == content2:
                             quotes2[next_id].pop(voice_desc2)
             _ship_info = kcdata_json[after_ship_id]
-    with open('quotes.json', 'w', encoding='utf-8') as fp:
+    with open('./quotes.json', 'w', encoding='utf-8') as fp:
         json.dump(quotes2, fp, ensure_ascii=False,
                   indent=2)
 
 
+def update_data():
+    print('[-] 正在更新数据……')
+    update_subtitles()
+    print('[1] 更新通常语音翻译成功！')
+    update_seasonal()
+    print('[2] 更新季节性语音翻译成功！')
+    print('[Y] 语音翻译数据更新成功！')
+
+
 if __name__ == '__main__':
-    updateSubtitles()
-    updateSeasonal(seasonal_suffix)
-    minifyAll()
+    if len(sys.argv) != 2:
+        print_help()
+        sys.exit(0)
+    cmd = sys.argv[1]
+    if cmd == '--fetch':
+        fetch_data()
+    elif cmd == '--update':
+        update_data()
+    else:
+        print_help()
+        sys.exit(0)
